de9dada7e7c8673ff14924238137090214e0ab89
Jessica Dussault <jdussault4@gmail.com>
2020-05-08 12:05:08 -0500

    creates orchid app and starts customizing search pages

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,8 @@
+// Compatible with Sprockets 3.x defaults to avoid breaking existing apps
+// https://github.com/rails/sprockets/blob/master/UPGRADING.md#manifestjs
+//= link_tree ../images
+//= link application.css
+//= link application.js
+
+// Orchid engine's manifest: orchid/app/assets/config/orchid/manifest.js
+//= link orchid/manifest.js
