dc637404e17a3b1bad4b9f4f697332bbeb0de116
Jessica Dussault <jdussault4@gmail.com>
2020-05-11 13:33:41 -0500

    sets up audio for on parade / marches

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # general
   get "timeline", to: "general#timeline", as: "timeline"
 
+  # multimedia
   get "multimedia", to: "multimedia#index", as: "multimedia"
+  get "multimedia/audio", to: "multimedia#audio", as: "audio"
+  get "multimedia/audio/:id", to: "multimedia#audio_item", as: "audio_item"
+  get "multimedia/documents", to: "multimedia#documents", as: "documents"
 
+  # stories
   get "stories", to: "stories#index", as: "stories"
   get "stories/accounts", to: "stories#accounts", as: "accounts"
   get "stories/accounts/:id", to: "stories#account", as: "account"
