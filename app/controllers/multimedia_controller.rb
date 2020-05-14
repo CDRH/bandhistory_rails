58f304151cfd9d8c2cfb960ecc9368121737c364
Jessica Dussault <jdussault4@gmail.com>
2020-05-14 11:58:00 -0500

    adds image gallery using search preset functionality
    relies upon test feature in orchid branch search_presets

--- app/controllers/multimedia_controller.rb
+++ app/controllers/multimedia_controller.rb
@@ -1,3 +1,28 @@
 class MultimediaController < ApplicationController
 
+  def image
+    @res = $api.get_item_by_id(params["id"]).first
+  end
+
+  def images
+    @title = "Images"
+
+    options = params.permit!.deep_dup
+    options["f"] = [] if options["f"].blank?
+    options["f"] << "subcategory|Images"
+    if params["topic"].present?
+      options["f"] << "topics|#{params["topic"]}"
+    end
+
+    @facets = $api.query({
+      "facet" => "topics",
+      "facet_sort" => "term|asc",
+      "num" => 0,
+      "f" => ["subcategory|Images"]
+    })
+    @res = $api.query(options)
+    @route_path = "images_path"
+    render_overridable "items", "search_preset"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   get "multimedia/audio", to: "multimedia#audio", as: "audio"
   get "multimedia/audio/:id", to: "multimedia#audio_item", as: "audio_item"
   get "multimedia/documents", to: "multimedia#documents", as: "documents"
+  get "multimedia/images", to: "multimedia#images", as: "images"
+  # note: topic has same path as ID so need constraint on matches
+  get "multimedia/images/:topic", to: "multimedia#images", as: "image_topic",
+    topic: /(aerial|field|group|parade|people)/
+  get "multimedia/images/:id", to: "multimedia#image", as: "image"
 
   # stories
   get "stories", to: "stories#index", as: "stories"
