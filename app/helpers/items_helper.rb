4db0d16a43eb1f698324f4a4c84c59aeca739f45
Jessica Dussault <jdussault4@gmail.com>
2020-05-19 14:26:06 -0500

    setting up search results for stories

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -10,9 +10,14 @@ module ItemsHelper
       path = image_path(id: item["identifier"])
     elsif subcategory == "footage"
       path = footage_clip_path(id: item["identifier"])
+    elsif subcategory == "personal accounts"
+      id = item["identifier"].split(".").last
+      path = account_path(id: id)
+    elsif subcategory == "features"
+      id = item["identifier"].split(".").last
+      path = feature_path(id: id)
     end
 
-    puts "PATH #{path}"
     link_to title_display, path
   end
 
