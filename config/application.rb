e2e32d63da05b47b75d97cf0afef03fb8249e365
William Dewey <wdewey2@unl.edu>
2024-10-30 16:10:43 -0500

    run rails/app:update

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module BandhistoryRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    # Enable templates for error pages rather than static HTML files
-    config.exceptions_app = self.routes
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
